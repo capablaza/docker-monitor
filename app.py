82775fe97ad7baf7561d8d549f7152265d2c2fe0
carlos apablaza <c.apablaza.z@gmail.com>
2019-08-22 00:53:14 -0400

    fix container for get all informations

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from docker_func import *
 from docker.errors import APIError
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route("/containers_names", methods=['GET'])
-def getContainerNames():
+def get_container_names():
     try:
-        return get_containers_names()
+        return jsonify(get_containers_names())
     except APIError as error:
         return {
             error.response.status_code :  error.response.reason
@@ -15,15 +16,24 @@ def getContainerNames():
 
 
 @app.route("/logs/<container_name>", methods=['GET'])
-def getLogByContainerName(container_name):
+def get_log_by_container_name(container_name):
     try:
-        logContainer = get_log_container_name(container_name)
-        print(str(logContainer))
+        logContainer = get_log_container_name(container_name)        
         return logContainer
     except APIError as error:
         return {
             error.response.status_code :  error.response.reason
         }
 
+@app.route("/image_names", methods=['GET'])
+def get_images_names():
+    try:
+         return jsonify(get_images_name())
+    except APIError as error:
+        return {
+            error.response.status_code :  error.response.reason
+        }
+
 if __name__ == '__main__':
+    get_images_name()
     app.run(host='0.0.0.0')
--- docker_func.py
+++ docker_func.py
@@ -1,13 +1,18 @@
 import docker
+import json
 
 def get_containers_names():
     client = docker.from_env()
 
     containers = client.containers.list(all)
-    list_containers = dict()
+    list_containers = []
 
     for con in containers:
-        list_containers = con.name
+        con_obj = {
+            "name" : con.name ,
+            "status" : con.status            
+        }        
+        list_containers.append(con_obj)        
     return list_containers
 
 def get_log_container_name(container_name):
@@ -15,5 +20,15 @@ def get_log_container_name(container_name):
     container = client.containers.get(container_name)
     list_logs = dict()
     list_logs = container.logs()
-
     return list_logs
+
+def get_images_name():
+    client = docker.from_env()
+    images_from_system = client.images.list(all=True)
+    list_images = []
+    for image in images_from_system:
+        img_con = {
+            "name" : image.tags
+        }
+        list_images.append(img_con)    
+    return list_images
